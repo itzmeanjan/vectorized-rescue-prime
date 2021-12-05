37e26ccf1618a218846807bb4bf3b9789bebcf49
Anjan Roy <anjanroy@yandex.com>
2021-12-05 14:28:03 +0000

    test case for apply_inv_sbox device function

--- test_rescue_prime.c
+++ test_rescue_prime.c
@@ -61,5 +61,73 @@ cl_int test_apply_sbox(cl_context ctx, cl_command_queue cq, cl_kernel krnl) {
   clReleaseMemObject(in_buf);
   clReleaseMemObject(out_buf);
 
+  printf("passed apply_sbox tests !\n");
+
+  return status;
+}
+
+cl_int test_apply_inv_sbox(cl_context ctx, cl_command_queue cq,
+                           cl_kernel krnl) {
+  cl_int status;
+
+  uint64_t in_arr[16] = {1ull << 10, 1ull << 11, 1ull << 12, 1ull << 13,
+                         1ull << 20, 1ull << 21, 1ull << 22, 1ull << 23,
+                         1ull << 60, 1ull << 61, 1ull << 62, 1ull << 63,
+                         0ull,       0ull,       0ull,       0ull};
+  uint64_t out_arr[16] = {0ull};
+  uint64_t exp_out_arr[16] = {18446743794536677441ull,
+                              536870912ull,
+                              4503599626321920ull,
+                              18446735273321562113ull,
+                              18446726477228539905ul,
+                              8ull,
+                              288230376151711744ull,
+                              18446744069414453249ull,
+                              68719476736ull,
+                              576460752169205760ull,
+                              18445618169507741697ull,
+                              512ull,
+                              0ull,
+                              0ull,
+                              0ull,
+                              0ull};
+
+  cl_mem in_buf = clCreateBuffer(ctx, CL_MEM_READ_ONLY, sizeof(cl_ulong) * 16,
+                                 NULL, &status);
+  cl_mem out_buf = clCreateBuffer(ctx, CL_MEM_WRITE_ONLY, sizeof(cl_ulong) * 16,
+                                  NULL, &status);
+
+  status = clSetKernelArg(krnl, 0, sizeof(cl_mem), &in_buf);
+  status = clSetKernelArg(krnl, 1, sizeof(cl_mem), &out_buf);
+
+  cl_event evt_0;
+  status = clEnqueueWriteBuffer(cq, in_buf, CL_FALSE, 0, sizeof(in_arr), in_arr,
+                                0, NULL, &evt_0);
+
+  size_t global_size[] = {1};
+  size_t local_size[] = {1};
+
+  cl_event evt_1;
+  status = clEnqueueNDRangeKernel(cq, krnl, 1, NULL, global_size, local_size, 1,
+                                  &evt_0, &evt_1);
+
+  cl_event evt_2;
+  status = clEnqueueReadBuffer(cq, out_buf, CL_FALSE, 0, sizeof(out_arr),
+                               out_arr, 1, &evt_1, &evt_2);
+
+  status = clWaitForEvents(1, &evt_2);
+
+  for (size_t i = 0; i < 16; i++) {
+    assert(out_arr[i] % MOD == exp_out_arr[i]);
+  }
+
+  clReleaseEvent(evt_0);
+  clReleaseEvent(evt_1);
+  clReleaseEvent(evt_2);
+  clReleaseMemObject(in_buf);
+  clReleaseMemObject(out_buf);
+
+  printf("passed apply_inv_sbox tests !\n");
+
   return status;
 }
